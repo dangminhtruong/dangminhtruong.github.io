03217f84a0d19b9299986a49c08f329abb3cc911
dangminhtruong <xdangminhtruongx@gmail.com>
2020-03-29 17:20:17 +0700

    Updates

---
+++ precache-manifest.53f671548b763805f84bd12a04546720.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5390bd7f3c1d0a20fb8324d626610811",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3899f4039f2c6483776c",
+    "url": "/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "7c5b3e9300af0bc621fa",
+    "url": "/static/css/4.08f3212a.chunk.css"
+  },
+  {
+    "revision": "3e01cc1975a9c88926ed",
+    "url": "/static/css/main.608da7e9.chunk.css"
+  },
+  {
+    "revision": "3899f4039f2c6483776c",
+    "url": "/static/js/2.37e910aa.chunk.js"
+  },
+  {
+    "revision": "b372f0359f71a6f618e2",
+    "url": "/static/js/3.e7771658.chunk.js"
+  },
+  {
+    "revision": "7c5b3e9300af0bc621fa",
+    "url": "/static/js/4.a9df94fc.chunk.js"
+  },
+  {
+    "revision": "3e01cc1975a9c88926ed",
+    "url": "/static/js/main.a80b3a63.chunk.js"
+  },
+  {
+    "revision": "69e3931aeb08173ac597",
+    "url": "/static/js/runtime~main.813761f2.js"
+  }
+]);
